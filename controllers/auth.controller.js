454625bc1afaf5927de2e6aec81293d43dbb38fc
devdiagon <fredericktimo@gmail.com>
2026-06-18 12:23:20 -0500

    feat: simular endpoints independientes para service-alpha y service-beta

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -5,11 +5,14 @@ export class AuthController {
      * Simula un servidor de autenticación que genera un token.
      */
     static async generateToken(req, res) {
-        // TODO: Implementar generación de token.
-        // 1. Obtener credenciales (username/password) del req.body.
-        // 2. Validar credenciales de forma simulada (ej. if user === 'admin'...).
-        // 3. Si son válidas, crear un payload y llamar a JwtService.signToken(payload).
-        // 4. Responder con el token generado.
-        // 5. Si son inválidas, responder con status 401.
+        const { username, password } = req.body;
+
+        if (username !== 'admin' || password !== 'admin123') {
+            return res.status(401).json({ message: 'Usuario o contraseña incorrectos' });
+        }
+
+        const payload = { sub: username, role: 'admin' };
+        const token = JwtService.signToken(payload);
+        res.json({ token });
     }
 }
