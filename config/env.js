c5e79be8edeb73a84ada64251a74890ccb7ea81b
devdiagon <fredericktimo@gmail.com>
2026-06-20 11:29:16 -0500

    feat: agregar variables de entorno nuevas a env.js

--- config/env.js
+++ config/env.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import fs from 'fs';
+import pkg from '../package.json' with { type: 'json' };
 
 dotenv.config();
 
@@ -8,5 +9,8 @@ export const config = {
     JWT_SECRET: process.env.JWT_SECRET,
     PRIVATE_KEY: process.env.PRIVATE_KEY_PATH ? fs.readFileSync(process.env.PRIVATE_KEY_PATH, 'utf8') : null,
     PUBLIC_KEY: process.env.PUBLIC_KEY_PATH ? fs.readFileSync(process.env.PUBLIC_KEY_PATH, 'utf8') : null,
-    ALGORITHM: process.env.JWT_ALGORITHM || 'HS256' // 'HS256' o 'RS256'
+    ALGORITHM: process.env.JWT_ALGORITHM || 'HS256', // 'HS256' o 'RS256'
+    SENTRY_DSN: process.env.SENTRY_DSN || '',
+    SENTRY_RELEASE: process.env.SENTRY_RELEASE || `${pkg.name}@${pkg.version}`,
+    ENVIRONMENT: process.env.NODE_ENV || 'development',
 };
