ea1dd071942b1eeba98d1a2e4d881c28ca7106e5
devdiagon <fredericktimo@gmail.com>
2026-06-20 12:12:17 -0500

    feat: implementación de fallo con tags en llamada a service-beta

--- controllers/resource.controller.js
+++ controllers/resource.controller.js
@@ -12,7 +12,26 @@ export class ResourceController {
     /**
      * Simula un recurso privado del Microservicio Beta.
      */
-    static getBetaPrivateData(req, res) {
-        res.json({ message: 'Acceso exitoso al Microservicio Beta', user: req.user });
+    static getBetaPrivateData(req, res, next) {
+        try {
+            throw new Error('Ocurrió un error inesperado en el Microservicio Beta');
+            //res.json({ message: 'Acceso exitoso al Microservicio Beta', user: req.user });
+        } catch (err) {
+            const user = req.user ?? {};
+
+            Sentry.captureException(err, {
+                tags: {
+                    microservice: 'beta',
+                    method: 'get private data',
+                },
+                extra: {
+                    user: user.sub ?? user.id ?? 'desconocido',
+                    emitedAt: user.iat ? new Date(user.iat * 1000).toISOString() : null,
+                    expiresAt: user.exp ? new Date(user.exp * 1000).toISOString() : null,
+                },
+            });
+
+            next(err);
+        }
     }
 }
